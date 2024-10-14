7bfe482b1923262fa9686e25d921abca1bae0b9c
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-14 13:52:45 +0700

    Membuat module findContact untuk detail contact - video 17

--- 17-express-contact-app/utils/contacts.js
+++ 17-express-contact-app/utils/contacts.js
@@ -12,11 +12,18 @@ if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8')
 }
 
-// load contact
+// ambil semua data di contact.json
 const loadContact = () => {
     const file = fs.readFileSync('data/contacts.json', 'utf8');
     const contacts = JSON.parse(file);
     return contacts
 }
 
-module.exports = {loadContact}
+// cari contact berdasarkan nama
+const findContact = (nama) => {
+    const contacts = loadContact();
+    const contact = contacts.find((contact) => contact.nama.toLowerCase() === nama.toLowerCase())
+    return contact
+}
+
+module.exports = {loadContact, findContact}
